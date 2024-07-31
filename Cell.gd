b176e0441ee6c0205d1e837a64c2cced7bf087b1
Steph Kraemer <purple.stephyr@proton.me>
2024-07-30 21:05:12 -0400

    Add ability to type numbers

--- Cell.gd
+++ Cell.gd
@@ -18,7 +18,8 @@
 extends Area2D
 
 signal clicked
-signal selected
+
+var selected = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -29,15 +30,38 @@ func _ready():
 func _process(delta):
 	pass
 
+func _input(event):
+	if selected:
+		if event.is_action_pressed("type_1"):
+			$Label.text = "1"
+		elif event.is_action_pressed("type_2"):
+			$Label.text = "2"
+		elif event.is_action_pressed("type_3"):
+			$Label.text = "3"
+		elif event.is_action_pressed("type_4"):
+			$Label.text = "4"
+		elif event.is_action_pressed("type_5"):
+			$Label.text = "5"
+		elif event.is_action_pressed("type_6"):
+			$Label.text = "6"
+		elif event.is_action_pressed("type_7"):
+			$Label.text = "7"
+		elif event.is_action_pressed("type_8"):
+			$Label.text = "8"
+		elif event.is_action_pressed("type_9"):
+			$Label.text = "9"
+
 
 func unselect():
 	print("Unselecting ", self)
 	$Sprite2D.texture = load("res://art/cell_unselected.png")
+	selected = false
 
 
 func select():
 	print("Selecting ", self)
 	$Sprite2D.texture = load("res://art/cell_selected.png")
+	selected = true
 
 
 func _on_input_event(viewport, event, shape_idx):
